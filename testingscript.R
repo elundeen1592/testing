65a855c7262e002f87684087a59c4a35288cbb10
Elizabeth Lundeen <yxj4@cdc.gov>
2022-02-05 12:29:23 -0500

    Initial commit

--- testingscript.R
+++ testingscript.R
@@ -1,3 +1,10 @@
 print("This file was created within RStudio")
 print("And now it lives on GitHub")
 str(iris)
+usethis::create_github_token()
+
+gitcreds::gitcreds_set()
+
+usethis::gh_token_help()
+usethis::git_sitrep()
+str(iris)
